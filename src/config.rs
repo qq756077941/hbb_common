3fb7c91aacf39ad6985d8d20500674ce23bf3e97
qq756077941 <125112321+qq756077941@users.noreply.github.com>
2025-06-18 22:01:58 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["110.40.203.117"];
+pub const RS_PUB_KEY: &str = "Q3sqkvgtNWfoAA3G58lulIX7uMQX45M6voBa7qphOAU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
